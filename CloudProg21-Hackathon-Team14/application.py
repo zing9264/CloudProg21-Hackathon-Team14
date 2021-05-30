697e92f589e089fda3e70b4876a9f9f463136efc
vinhuang0228 <vinhuang0228@gmail.com>
2021-05-30 13:11:23 +0800

    fix login and jinja show (還要修比對登入資訊，是我的店才能改資料）

--- CloudProg21-Hackathon-Team14/application.py
+++ CloudProg21-Hackathon-Team14/application.py
@@ -31,107 +31,101 @@ FLASK_DEBUG = 'false' if os.environ.get(
 
 # Create the Flask app
 application = flask.Flask(__name__)
-application.secret_key = 'CloudProg21Team14'
 # Load config values specified above
 application.config.from_object(__name__)
-
 # Load configuration vals from a file
 # application.config.from_envvar('APP_CONFIG', silent=True)
 application.config.from_pyfile('config.py')
-
+application.secret_key = application.config['SECRET_KEY']
 # Only enable Flask debugging if an env var is set to true
 application.debug = application.config['FLASK_DEBUG'] in ['true', 'True']
 
+# ============================== login =================================
 login_manager = LoginManager()
 login_manager.init_app(application)
 login_manager.session_protection = "strong"
-login_manager.login_view = 'login'
+login_manager.login_view = 'loginpage'
 class User(UserMixin):
     pass
 
-def check_user(store_id):
+def check_user(id):
     dynamodb = boto3.resource(
         'dynamodb', region_name=application.config['AWS_REGION'])
-    table = dynamodb.Table(application.config['STORE_LIST'])
+    table = dynamodb.Table(application.config['USER_LIST'])
     try:
-        res = table.get_item(Key={'id': store_id})
+        res = table.get_item(Key={'id': id})
         return res['Item']
     except:
         print("User doesn't exist")
         return None
         
 @login_manager.user_loader
-def load_user(store_id):
-    if check_user(store_id) is not None:
+def load_user(id):
+    if check_user(id) is not None:
         curr_user = User()
-        curr_user.id = store_id
+        curr_user.id = id
     return curr_user
-
-
-
-# # LET USER LOGIN FIRST
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        flash('You need to login first.')
-        return redirect(url_for('login'))
-    return wrap
-
-
-#這塊需要從資料庫撈資料 並用jinja2 渲染到前端
-@application.route('/')
-def welcome():
-    theme = application.config['THEME']
-    return flask.render_template('index.html', theme=theme, flask_debug=application.debug)
-
+    
 @application.route('/sign_up')
 def sign_uppage():
     return flask.render_template('sign_up_restaurant.html', flask_debug=application.debug)
-
-# @application.route('/login')
-# def loginpage():
-#     return flask.render_template('login.html', flask_debug=application.debug)
 
 @application.route('/login', methods=['GET','POST'])
 def loginpage():
     id = request.form.get('inputEmail')
     password = request.form.get('inputPassword')
     remember = True if request.form.get('remember') else False
-    print(id)
+    # print(id)
     user = check_user(id)
-    if user is not None and request.form['password'] == user['password']:
+    if user is not None and password ==user['password']:
         curr_user = User()
         curr_user.id = id
         print(curr_user)
-        login_user(curr_user)
-
-        return redirect(url_for('welcome'))
-        # session['logged_in'] = True
-        # return Response(password, status=201, mimetype='application/json')
+        login_user(curr_user,remember=remember)
+        next = request.args.get('next')
+        print(request.args)
+        if not next_is_valid(next):
+            #  如果使用者沒有該url權限，那就reject掉。
+            return 'Bad Boy!!'
+        return redirect(next or url_for('welcome'))
     else:
         flash('Wrong username or password!')
         
     return flask.render_template('login.html', flask_debug=application.debug)
-
+def next_is_valid(url):
+    return True
 # define logout
-
-
 @application.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     session.pop('logged_in', None)
     flash('You already logout !!!')
     return redirect(url_for('welcome'))
+# ============================== login =================================
+
+# #這塊需要從資料庫撈資料 並用jinja2 渲染到前端
+@application.route('/')
+def welcome():
+    theme = application.config['THEME']
+    store_list = get_all_store_DBitem(application.config['STORE_INFO'])
+    return flask.render_template('index.html', theme=theme, flask_debug=application.debug , stores = store_list)
 
 
 #店家詳細資料這塊需要傳入所有資料庫變數，用jinja2 render出來
 @application.route('/storepage/<storename>')
 def storepage(storename):
     print(storename)
-    return flask.render_template('storepage.html', storename=storename, flask_debug=application.debug)
-
+    storeinfo =get_DBitem(application.config['STORE_INFO'],storename)
+    contact = json.loads(storeinfo['contact'])   # dict
+    # turn dict to 2D array in order to use jinja to show the info in DB
+    normal = dict_to_2Darray(json.loads(storeinfo['normal']))     
+    discount = dict_to_2Darray(json.loads(storeinfo['discount']))  
+    return flask.render_template('storepage.html',
+                                flask_debug=application.debug,
+                                info = storeinfo,
+                                contact = contact,
+                                normal = normal,
+                                discount = discount)
 
  
 ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg'}
@@ -140,12 +134,14 @@ def allowed_file(filename):
 
 #上傳圖片頁面
 @application.route('/storepage/<storename>/upload_file_page')
+@login_required
 def upload_file_page(storename):
     print(storename)
     return flask.render_template('upload_image.html', storename=storename, flask_debug=application.debug)
 
 #上傳圖片功能，會存檔到UPLOAD_FOLDER='static/uploads/' 之後要改成boto3 S3 bucket
 @application.route('/storepage/<storename>/image_upload', methods=['POST'])
+@login_required
 def storeimage_upload(storename):
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -173,24 +169,30 @@ def display_image(filename):
 
 #更新店家資料頁面 這塊需要傳入所有資料庫變數，用jinja2 render出來
 @application.route('/storepage/<storename>/update_storepage')
+@login_required
 def storeupdate_storepage(storename):
-    print(storename)
+    userinfo = check_user(current_user.id)
+    print(userinfo)
+    # if userinfo['storename'] == storename:
+    #     return flask.render_template('update_storepage.html', storename=storename, flask_debug=application.debug)
+    # else:
+    #     flash('This is not your store !!!')
+    #     return flask.render_template('storepage.html', storename=storename, flask_debug=application.debug)
     return flask.render_template('update_storepage.html', storename=storename, flask_debug=application.debug)
-
 
 #更新店家資料功能 這塊需要驗證登入  這部分前端動態處理還沒完成
 @application.route('/storepage/<storename>/updateFormPost')
+@login_required
 def update_DBdata(storename):
     update_data = dict()
     item_list = []
+    data = {}
     for item in request.form:
-        signup_data[item] = request.form[item]
+        data[item] = request.form[item]
         item_list.append(item)
-    print(update_data)
+    print(data)
     print(item_list)
     return flask.render_template('storepage.html', storename=storename, flask_debug=application.debug)
-
-
 
 #註冊帳號與所有資料
 @application.route('/signupFormPost', methods=['POST'])
@@ -201,7 +203,8 @@ def signupFormPost():
         signup_data[item] = request.form[item]
         item_list.append(item)
     signup_data_parse(signup_data, item_list)
-    return Response(json.dumps(signup_data), status=201, mimetype='application/json')
+    flash('SIGN UP SUCCESS')
+    return redirect(url_for('welcome'))
 
 #註冊帳號用的語法分析器
 def signup_data_parse(data, item_list):
@@ -218,7 +221,8 @@ def signup_data_parse(data, item_list):
     user = {}
     user['id'] = data['inputEmail']
     user['password'] = data['inputPassword']
-    table = dynamodb.Table(application.config['STORE_LIST'])
+    user['storename'] = data['inputstore']
+    table = dynamodb.Table(application.config['USER_LIST'])
     response = table.put_item(Item=user)
     print("user info", response)
     # store info table
@@ -244,7 +248,35 @@ def signup_data_parse(data, item_list):
 
     return
 
+@application.route('/test')
+def test():
+    store_list = get_all_store_DBitem(application.config['STORE_INFO'])
+    storename = 'KFC'
+    storeinfo =get_DBitem(application.config['STORE_INFO'],storename)
+    contact = json.loads(storeinfo['contact'])   # dict
+    normal = dict_to_2Darray(json.loads(storeinfo['normal']))     
+    discount = dict_to_2Darray(json.loads(storeinfo['discount']))  
+    return flask.render_template('test.html', 
+            flask_debug=application.debug,
+            info = storeinfo,
+            contact = contact,
+            normal = normal,
+            discount = discount)
+    
+def get_all_store_DBitem(tablename):
+    dynamodb = boto3.resource('dynamodb', region_name=application.config['AWS_REGION'])
+    table = dynamodb.Table(tablename)
+    res = table.scan()
+    store_info_list = res['Items']
+    return store_info_list
 
+def dict_to_2Darray(dict_data):
+    array_2D =[]
+    for item in dict_data:
+        a = [item ,dict_data[item]]
+        array_2D.append(a)
+    return array_2D
+    
 def add_DBitem(tablename, item):
     # it's easy to update or add (update item just use same key)
     dynamodb = boto3.resource(
@@ -254,32 +286,29 @@ def add_DBitem(tablename, item):
     print(response)
     return
 
-
-def delete_DBitem(tablename, store_name):
-    # store_name must be string , use key "store" to delete item
+def delete_DBitem(tablename, storename):
+    # storename must be string , use key "store" to delete item
     dynamodb = boto3.resource(
         'dynamodb', region_name=application.config['AWS_REGION'])
     table = dynamodb.Table(tablename)
-    response = table.delete_item(Key={'store': store_name})
+    response = table.delete_item(Key={'store': storename})
     print(response)
     return
 
 
-def get_DBitem(tablename, store_name):
+def get_DBitem(tablename, storename):
     dynamodb = boto3.resource(
         'dynamodb', region_name=application.config['AWS_REGION'])
     table = dynamodb.Table(tablename)
-    res = table.get_item(Key={'store': store_name})
+    res = table.get_item(Key={'store': storename})
     item = res['Item']
     # json.loads(item['contact']) --> to load json
     # item['tag'][0] --> to get tag
     return item
 
 # parse example
-
-
-def parse_db_item(store_name):
-    item = get_DBitem(application.config['STORE_INFO'], store_name)
+def parse_db_item(storename):
+    item = get_DBitem(application.config['STORE_INFO'], storename)
     person_now = item['person_now']         # int?
     person_max = item['person_max']         # int?
     contact = json.loads(item['contact'])   # dict
@@ -306,7 +335,8 @@ initTableItem = {
 }
 initUserList = {
     'id': 'HotPot@mail.com',
-    'password': '1111111'
+    'password': '1111111',
+    'storename':'HotPot'
 }
 # check resource exist
 
@@ -343,14 +373,14 @@ def check_or_create():
             ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10})
         # create user list table
         ddb_conn.create_table(
-            TableName=application.config['STORE_LIST'],
+            TableName=application.config['USER_LIST'],
             KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
             AttributeDefinitions=[
                 {'AttributeName': 'id', 'AttributeType': 'S'}],
             ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10})
     except:
         ddb_table = ddb_conn.Table(application.config['STORE_INFO'])
-        ddb_table = ddb_conn.Table(application.config['STORE_LIST'])
+        ddb_table = ddb_conn.Table(application.config['USER_LIST'])
         print("table exists")
 
     try:
@@ -363,7 +393,7 @@ def check_or_create():
     try:
         ddb_table = ddb_conn.Table(application.config['STORE_INFO'])
         ddb_table.put_item(Item=initTableItem)
-        ddb_table = ddb_conn.Table(application.config['STORE_LIST'])
+        ddb_table = ddb_conn.Table(application.config['USER_LIST'])
         ddb_table.put_item(Item=initUserList)
         print("init DB table")
     except:
